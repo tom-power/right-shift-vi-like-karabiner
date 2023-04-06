bb6e6a7e24d26efa97c36ec2f3b1e54325405f48
Tom Power <tp21@tuta.io>
2023-04-06 19:00:19 +0100

    tidy readme, install

--- install.sh
+++ install.sh
@@ -1,36 +1,11 @@
 #! /bin/bash
 
-set -e
-
-not_which() {
-  ! which "$1" >/dev/null
-}
-
-cask_install() {
-  brew cask install "$1"
-}
-
-if not_which brew; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-if not_which zsh; then
-  brew install zsh
-fi
-
-zsh_path="$(which zsh)"
-if [ "$(dscl . -read /Users/$(whoami) UserShell)" != "UserShell: $zsh_path" ]; then
-  if ! grep -q "$zsh_path" /etc/shells; then
-    # Allow (Homebrew's) Zsh to be set as the default shell
-    echo "$zsh_path" | sudo tee -a /etc/shells
-  fi
-
-  chsh -s "$zsh_path"
-fi
-
 if [ ! -d /Applications/Karabiner-Elements.app ]; then
-  cask_install karabiner-elements
+  echo "Karabiner-Elements not found, please install"
+  exit
 fi
+
+echo "Copying config.."
 
 configDir=~/.config/karabiner/assets/complex_modifications/
 
@@ -41,7 +16,8 @@ fi
 cp ./src/right_shift_vi_like.json $configDir
 
 if  [ "$1" == "--with-extras" ]; then
+  echo "copying extras.."
   cp ./src/extras/* $configDir
 fi
 
-echo "All done, remember to install the configurations in karabiner-elements preferences"
+echo "..all done, remember to the install mappings through Karabiner-Elements preferences"
